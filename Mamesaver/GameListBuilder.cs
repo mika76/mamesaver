fe0202cfecabe9ef573fa6fe6e4e2cfaa6dd7eb0
Matthew Painter <Matthew_Painter@corp.psis.co.nz>
2018-08-12 13:51:35 +1200

    Added parallelisation

--- Mamesaver/GameListBuilder.cs
+++ Mamesaver/GameListBuilder.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 using System.Xml;
 using System.Xml.Linq;
 
@@ -10,6 +11,11 @@ namespace Mamesaver
 {
     internal static class GameListBuilder
     {
+        private static readonly ParallelOptions ParallelOptions = new ParallelOptions
+        {
+            MaxDegreeOfParallelism = Environment.ProcessorCount
+        };
+
         /// <summary>
         /// Returns a <see cref="List{T}"/> of <see cref="SelectableGame"/>s which are read from
         /// the full list and then merged with the verified rom's list. The games which are returned
@@ -21,40 +27,42 @@ namespace Mamesaver
             var games = new List<SelectableGame>();
 
             // Enrich game metadata for each verified game
-            foreach (var game in GetVerifiedSets())
+            Parallel.ForEach(GetVerifiedSets(), ParallelOptions, game =>
             {
-                using (var stream = GetGameDetails(game.Key))
                 {
-                    var readerSettings = new XmlReaderSettings { DtdProcessing = DtdProcessing.Parse };
-                    using (var reader = XmlReader.Create(stream, readerSettings))
+                    using (var stream = GetGameDetails(game.Key))
                     {
-                        reader.ReadStartElement("mame");
-
-                        // Read each machine, enriching metadata for verified sets
-                        while (reader.Read() && reader.Name == "machine")
+                        var readerSettings = new XmlReaderSettings { DtdProcessing = DtdProcessing.Parse };
+                        using (var reader = XmlReader.Create(stream, readerSettings))
                         {
-                            // Read game metadata
-                            var element = (XElement) XNode.ReadFrom(reader);
+                            reader.ReadStartElement("mame");
 
-                            var name = element.Attribute("name")?.Value;
-                            if (name == null) continue;
+                            // Read each machine, enriching metadata for verified sets
+                            while (reader.Read() && reader.Name == "machine")
+                            {
+                                // Read game metadata
+                                var element = (XElement) XNode.ReadFrom(reader);
 
-                            var driver = element.Element("driver");
-                            if (driver == null) continue;
+                                var name = element.Attribute("name")?.Value;
+                                if (name == null) continue;
 
-                            // Skip games which aren't fully emulated
-                            var status = driver.Attribute("status")?.Value;
-                            if (status != "good") continue;
+                                var driver = element.Element("driver");
+                                if (driver == null) continue;
 
-                            var year = element.Element("year")?.Value ?? "";
-                            var manufacturer = element.Element("manufacturer")?.Value ?? "";
-                            var description = element.Element("description")?.Value ?? "";
+                                // Skip games which aren't fully emulated
+                                var status = driver.Attribute("status")?.Value;
+                                if (status != "good") continue;
 
-                            games.Add(new SelectableGame(name, description, year, manufacturer, false));
+                                var year = element.Element("year")?.Value ?? "";
+                                var manufacturer = element.Element("manufacturer")?.Value ?? "";
+                                var description = element.Element("description")?.Value ?? "";
+
+                                games.Add(new SelectableGame(name, description, year, manufacturer, false));
+                            }
                         }
                     }
                 }
-            }
+            });
 
             return games;
         }
@@ -70,18 +78,18 @@ namespace Mamesaver
             var regex = new Regex(@"romset (\w*)(?:\s\[(\w*)\])? is good"); //only accept the "good" ROMS
 
             // Verify each rom in directory
-            foreach (var rom in GetRomFiles())
+            Parallel.ForEach(GetRomFiles(), ParallelOptions, rom =>
             {
                 // Retrieve state per rom
                 using (var stream = MameInvoker.GetOutput("-verifyroms", rom))
                 {
                     var output = stream.ReadToEnd();
-                    if (!regex.IsMatch(output)) continue;
+                    if (!regex.IsMatch(output)) return;
 
                     var matches = regex.Match(output);
                     verifiedRoms[matches.Groups[1].Value] = matches.Groups[2].Value;
                 }
-            }
+            });
 
             return verifiedRoms;
         }
