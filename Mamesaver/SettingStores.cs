2b948a1b1f975c4c2aa199e088b4f032575841f6
Matthew Painter <Matthew_Painter@corp.psis.co.nz>
2018-08-24 20:42:14 +1200

    Added static container to access setting stores

---
+++ Mamesaver/SettingStores.cs
@@ -0,0 +1,23 @@
+﻿using Mamesaver.Settings;
+
+namespace Mamesaver
+{
+    public static class SettingStores
+    {
+        /// <summary>
+        ///     Game list and selection state
+        /// </summary>
+        public static GameListStore GameList { get; } 
+
+        /// <summary>
+        ///     User settings
+        /// </summary>
+        public static GeneralSettingsStore General { get; }
+
+        static SettingStores()
+        {
+            GameList = new GameListStore();
+            General = new GeneralSettingsStore();
+        }
+    }
+}
