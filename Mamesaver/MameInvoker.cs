dae6bab32e602cf0ad13a5a734d634ef1446a20f
Matt <matthewpainter@co-operativebank.co.nz>
2018-08-26 21:46:38 +1200

    Added logging, updated for Simple Injector confguration, commenting

--- Mamesaver/MameInvoker.cs
+++ Mamesaver/MameInvoker.cs
@@ -1,19 +1,29 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
+using Mamesaver.Configuration.Models;
+using Serilog;
 
 namespace Mamesaver
 {
-    internal static class MameInvoker
+    /// <summary>
+    ///     Invookes the MAME executable configured in <see cref="Settings"/>.
+    /// </summary>
+    internal class MameInvoker
     {
+        private readonly Settings _settings;
+
+        public MameInvoker(Settings settings) => _settings = settings;
+
         /// <summary>
-        ///     Invokes Mame, returning the created process
+        ///     Invokes MAME, returning the created process
         /// </summary>
         /// <param name="arguments">arguments to pass to Mame</param>
-        public static Process Run(params string[] arguments)
+        public Process Run(params string[] arguments)
         {
+            Log.Information("Invoking MAME with arguments: {arguments}", string.Join(" ", arguments));
 
-            var execPath = SettingStores.General.Get().ExecutablePath;
+            var execPath = _settings.ExecutablePath;
             var psi = new ProcessStartInfo(execPath)
             {
                 Arguments = string.Join(" ", arguments),
@@ -24,19 +34,24 @@ namespace Mamesaver
                 WindowStyle = ProcessWindowStyle.Hidden
             };
 
-            var p = Process.Start(psi);
-            if (p == null) throw new InvalidOperationException($"Mame process not created: {psi.FileName} {psi.Arguments}");
+            try
+            {
+                var process = Process.Start(psi);
+                if (process == null) throw new InvalidOperationException($"MAME process not created: {psi.FileName} {psi.Arguments}");
 
-            return p;
+                return process;
+            }
+            catch (Exception e)
+            {
+                Log.Error(e, "Unable to run MAME: {filename} {arguments}", psi.FileName, psi.Arguments);
+                throw;
+            }
         }
 
         /// <summary>
-        ///     Invokes Mame, returning the standard output stream
+        ///     Invokes MAME, returning the standard output stream
         /// </summary>
-        /// <param name="arguments">raguments to pass to Mame</param>
-        public static StreamReader GetOutput(params string[] arguments)
-        {
-            return Run(arguments).StandardOutput;
-        }
+        /// <param name="arguments">arguments to pass to MAME</param>
+        public StreamReader GetOutput(params string[] arguments) => Run(arguments).StandardOutput;
     }
 }
