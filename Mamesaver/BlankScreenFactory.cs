26bb61e5d78f6272add2acb4217bcd4f148623b6
Matt <matthewpainter@co-operativebank.co.nz>
2018-08-26 21:47:54 +1200

    Convenience factory for creating BlankScreens

---
+++ Mamesaver/BlankScreenFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Mamesaver
+{
+    /// <summary>
+    ///     Convenience factory to create blank screens.
+    /// </summary>
+    internal class BlankScreenFactory
+    {
+        private readonly Func<BlankScreen> _factory;
+
+        public BlankScreenFactory(Func<BlankScreen> factory) => _factory = factory;
+
+        /// <summary>
+        ///     Creates a new <see cref="BlankScreen"/>.
+        /// </summary>
+        public BlankScreen Create() => _factory();
+    }
+}
