579c74f5b0be3c263089657c8295a95ab0cd9e6f
Matt <matthewpainter@co-operativebank.co.nz>
2018-08-26 21:41:42 +1200

    Tidying, commenting, updates for Simple Injector, added support for custom font face/size

--- Mamesaver/Layout/LayoutConstants.cs
+++ Mamesaver/Layout/LayoutConstants.cs
@@ -2,7 +2,14 @@
 {
     public static class LayoutConstants
     {
+        /// <summary>
+        ///     Filename of bezel image used for game title information.
+        /// </summary>
         public const string TitleImage = "gameInfo.png";
+
+        /// <summary>
+        ///     MAME layout version we're supporting.
+        /// </summary>
         public const string Version = "2";
     }
 }
--- Mamesaver/Layout/TitleFactory.cs
+++ Mamesaver/Layout/TitleFactory.cs
@@ -5,27 +5,91 @@ using System.Drawing.Imaging;
 using System.Drawing.Text;
 using System.IO;
 using System.Windows.Forms;
+using Mamesaver.Configuration.Models;
 using Mamesaver.Layout.Models;
-using Mamesaver.Models;
-using Mamesaver.Models.Settings;
 
 namespace Mamesaver.Layout
 {
     /// <summary>
-    ///     Generates an image containing game metadata for display in a Mame layout.
+    ///     Generates an image containing game metadata for display in a MAME layout.
     /// </summary>
-    public static class TitleFactory
+    public class TitleFactory
     {
-        private static readonly Color BackgroundColour = Color.Black;
-        private static readonly Color TextColour = Color.FromArgb(255, 210, 210, 210);
+        private readonly Color _backgroundColour = Color.Black;
+        private readonly Settings _settings;
+        private readonly Color _textColour = Color.FromArgb(255, 210, 210, 210);
 
-    private class TitleFonts
-    {
-        public Font RegularFont { get; set; }
-        public Font BoldFont { get; set; }
+        public TitleFactory(Settings settings) => _settings = settings;
+
+        /// <summary>
+        ///     Returns the height of the bezel containing the rendered title
+        /// </summary>
+        public int GetBezelHeight(Game game) => GetBezelHeight(GetDescriptionSize(game));
+
+        /// <summary>
+        ///     Returns the height of the bezel containing the rendered title.
+        /// </summary>
+        /// <param name="titleSize">size of title text</param>
+        private int GetBezelHeight(Size titleSize) => (int)Math.Round(titleSize.Height * 1.3f);
+
+        /// <summary>
+        ///     Returns the size of the rendered game description text.
+        /// </summary>
+        private Size GetDescriptionSize(Game game)
+        {
+            var font = GetTitleFonts().BoldFont;
+            return TextRenderer.MeasureText(DescriptionText(game), font);
         }
 
-        private static TitleFonts GetTitleFonts()
+        /// <summary>
+        ///     Returns the size of the rendered game details text.
+        /// </summary>
+        private Size GetDetailSize(Game game)
+        {
+            var font = GetTitleFonts().RegularFont;
+            return TextRenderer.MeasureText(DetailText(game), font);
+        }
+
+        /// <summary>
+        ///     Render a layout image for a game. The image's width is determined by <see cref="monitorWidth" /> and height
+        ///     relative to the font height.
+        /// </summary>
+        public int Render(Game game, MameLayout layout, Stream outputStream, int monitorWidth)
+        {
+            var bezelHeight = GetBezelHeight(game);
+            var titleSize = GetDescriptionSize(game);
+            var fonts = GetTitleFonts();
+
+            using (var image = new Bitmap(monitorWidth, bezelHeight, PixelFormat.Format32bppArgb))
+            using (var graphics = Graphics.FromImage(image))
+            {
+                PrepareGraphics(graphics);
+
+                // Vertically centre text inside bezel
+                var yOffset = (int) Math.Round(bezelHeight / 2.0 - titleSize.Height / 2.0);
+
+                // Horizontally centre game on screen
+                var textWidth = GetDetailSize(game).Width + GetDescriptionSize(game).Width;
+                var xOffset = (int) Math.Round(monitorWidth / 2.0 - textWidth / 2.0);
+
+                // Render game description, manufacturer and year
+                TextRenderer.DrawText(graphics, DescriptionText(game), fonts.BoldFont, new Point(xOffset, yOffset),
+                    _textColour);
+                TextRenderer.DrawText(graphics, DetailText(game), fonts.RegularFont,
+                    new Point(xOffset + titleSize.Width, yOffset), _textColour);
+
+                graphics.Save();
+                image.Save(outputStream, ImageFormat.Png);
+            }
+
+            return bezelHeight;
+        }
+
+        private string DescriptionText(Game game) => game.Description;
+
+        private string DetailText(Game game) => $"| {game.Manufacturer} {game.Year}";
+
+        private TitleFonts GetTitleFonts()
         {
             var fontSettings = FontSettings();
 
@@ -36,59 +100,17 @@ namespace Mamesaver.Layout
             };
         }
 
-        public static int GetBezelHeight(Game game) => GetBezelHeight(GetTitleSize(game));
-
-        private static int GetBezelHeight(Size titleSize) => (int)Math.Round(titleSize.Height * 1.3f);
-
-        private static Size GetTitleSize(Game game)
+        private FontSettings FontSettings()
         {
-            var boldFont = GetTitleFonts().BoldFont;
-            return TextRenderer.MeasureText(game.Description, boldFont);
+            var layoutSettings = _settings.LayoutSettings.InGameTitles;
+            return layoutSettings.FontSettings;
         }
 
         /// <summary>
-        ///     Render a layout image for a game. The image's width is determined by <see cref="monitorWidth"/> and height
-        ///     relative to the font height.
-        /// </summary>
-        public static int Render(Game game, MameLayout layout, Stream outputStream, int monitorWidth)
-        {
-            var bezelHeight = GetBezelHeight(game);
-            var titleSize = GetTitleSize(game);
-            var fonts = GetTitleFonts();
-
-            using (var image = new Bitmap(monitorWidth, bezelHeight, PixelFormat.Format32bppArgb))
-            using (var graphics = Graphics.FromImage(image))
-            {
-                PrepareGraphics(graphics);
-
-                // Vertically centre text
-                var yOffset = (int)Math.Round(bezelHeight / 2.0 - titleSize.Height / 2.0);
-                var xOffset = layout.View.Screen.Bounds.X;
-
-                // Render game description, manufacturer and year
-                TextRenderer.DrawText(graphics, game.Description, fonts.BoldFont, new Point(xOffset, yOffset), TextColour);
-                TextRenderer.DrawText(graphics, $"| {game.Manufacturer} {game.Year}", fonts.RegularFont, new Point(xOffset + titleSize.Width, yOffset), TextColour);
-
-                graphics.Save();
-                image.Save(outputStream, ImageFormat.Png);
-            }
-
-            return bezelHeight;
-        }
-
-        private static FontSettings FontSettings()
-        {
-            var settings = SettingStores.General.Get();
-            var layoutSettings = settings.LayoutSettings.InGameTitles;
-            var fontSettings = layoutSettings.FontSettings;
-            return fontSettings;
-        }
-
-        /// <summary>
-        ///     Adjusts a <see cref="Graphics"/> object for high quality rendering
+        ///     Adjusts a <see cref="Graphics" /> object for high quality rendering.
         /// </summary>
         /// <param name="graphics"></param>
-        private static void PrepareGraphics(Graphics graphics)
+        private void PrepareGraphics(Graphics graphics)
         {
             graphics.CompositingQuality = CompositingQuality.HighQuality;
             graphics.InterpolationMode = InterpolationMode.HighQualityBilinear;
@@ -96,7 +118,13 @@ namespace Mamesaver.Layout
             graphics.SmoothingMode = SmoothingMode.HighQuality;
             graphics.TextRenderingHint = TextRenderingHint.AntiAliasGridFit;
 
-            graphics.Clear(BackgroundColour);
+            graphics.Clear(_backgroundColour);
+        }
+
+        private class TitleFonts
+        {
+            public Font RegularFont { get; set; }
+            public Font BoldFont { get; set; }
         }
     }
 }
