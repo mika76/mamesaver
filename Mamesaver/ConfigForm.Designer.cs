ea79161a7b3daffa5d7b223318c0bae8808e9712
Matt Painter <matthewpainter@co-operativebank.co.nz>
2018-08-28 11:22:55 +1200

    Fixed Reset to Defaults button anchoring

--- Mamesaver/ConfigForm.Designer.cs
+++ Mamesaver/ConfigForm.Designer.cs
@@ -577,7 +577,7 @@ namespace Mamesaver
             // 
             // resetToDefaults
             // 
-            this.resetToDefaults.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.resetToDefaults.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.resetToDefaults.DialogResult = System.Windows.Forms.DialogResult.Cancel;
             this.resetToDefaults.Location = new System.Drawing.Point(12, 381);
             this.resetToDefaults.Name = "resetToDefaults";
