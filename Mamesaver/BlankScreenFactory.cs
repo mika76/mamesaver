c4736abc7280c629a5e02ce93ca4d0120b35b8b2
Matt <matthewpainter@co-operativebank.co.nz>
2018-08-30 16:31:12 +1200

    Updated with new constructor requirements

--- Mamesaver/BlankScreenFactory.cs
+++ Mamesaver/BlankScreenFactory.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Mamesaver.Configuration.Models;
+﻿using Mamesaver.Configuration.Models;
 
 namespace Mamesaver
 {
@@ -9,15 +8,17 @@ namespace Mamesaver
     internal class BlankScreenFactory
     {
         private readonly LayoutSettings _layoutSettings;
+        private readonly Settings _settings;
 
-        public BlankScreenFactory(LayoutSettings layoutSettings) => _layoutSettings = layoutSettings;
+        public BlankScreenFactory(LayoutSettings layoutSettings, Settings settings)
+        {
+            _layoutSettings = layoutSettings;
+            _settings = settings;
+        }
 
         /// <summary>
         ///     Creates a new <see cref="BlankScreen"/>.
         /// </summary>
-        public BlankScreen Create()
-        {
-            return new BlankScreen(new BackgroundForm(_layoutSettings));
-        }
+        public BlankScreen Create() => new BlankScreen(new BackgroundForm(_layoutSettings), _settings);
     }
 }
