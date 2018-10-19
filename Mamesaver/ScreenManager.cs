9d70aef8435865478531f23254b7bd655dd69c4f
Matt <matthewpainter@co-operativebank.co.nz>
2018-10-19 21:12:10 +1300

    Prevented crash if user activity occurs during shutdown

--- Mamesaver/ScreenManager.cs
+++ Mamesaver/ScreenManager.cs
@@ -61,7 +61,15 @@ namespace Mamesaver
             _activityHook.OnMouseActivity -= OnMouseActivity;
 
             Log.Debug("Exiting due to mouse activity");
-            _cancellationTokenSource.Cancel();
+
+            try
+            {
+                _cancellationTokenSource.Cancel();
+            }
+            catch (ObjectDisposedException)
+            {
+                // Ignored
+            }
         }
 
         /// <summary>
@@ -72,7 +80,15 @@ namespace Mamesaver
             _hotKeyManager.UnhandledKeyPressed -= UnhandledKeyPressed;
 
             Log.Debug("Exiting due to unhandled keypress");
-            _cancellationTokenSource.Cancel();
+
+            try
+            {
+                _cancellationTokenSource.Cancel();
+            }
+            catch (ObjectDisposedException)
+            {
+                // Ignored
+            }
         }
 
         /// <summary>
